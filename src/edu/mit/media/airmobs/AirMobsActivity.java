4c0f7636ea7713fc9b84c5306ed9f608fbbee847
Maksim Stepanenko <maksimus16@gmail.com>
2011-12-07 14:58:55 -0500

    empty android project

---
+++ src/edu/mit/media/airmobs/AirMobsActivity.java
@@ -0,0 +1,13 @@
+package edu.mit.media.airmobs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AirMobsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
